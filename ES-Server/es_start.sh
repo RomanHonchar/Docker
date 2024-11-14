345dfe3637b366e8e4a0334ba8cfbb02af46652d
RomanHonchar <141116263+RomanHonchar@users.noreply.github.com>
2024-11-14 15:18:33 +0200

    Update es_start.sh

--- ES-Server/es_start.sh
+++ ES-Server/es_start.sh
@@ -8,7 +8,7 @@ ES_NAME=${values[3]}
 
 export MONO_IOMAP=all
 /bin/rm -f /tmp/QsExecutionServer.exe.lock
-mono /opt/ExecutionServer/QsExecutionServerConsoleConfig.exe /s:$CS_IP /u:$CS_U /p:$CS_P /esn:$ES_NAME /i:100 
+mono /opt/ExecutionServer/QsExecutionServerConsoleConfig.exe /s:$CS_IP /u:$CS_U /p:$CS_P /esn:$ES_NAME /i:100 /ansible
 unset PARAMS
 export
 mono-service -d:/opt/ExecutionServer /opt/ExecutionServer/QsExecutionServer.exe --no-daemon
